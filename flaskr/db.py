e63235cd7ead2ca7f5c7452f50db69887b056993
Amon Peide Ng <peide.amon.ng@gmail.com>
2018-05-20 17:12:40 +1200

    added flask_app

--- flaskr/db.py
+++ flaskr/db.py
@@ -9,10 +9,12 @@ app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    # DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='default',
+    FLASK_APP='flaskr',
+    FLASK_DEBUG = 1
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
